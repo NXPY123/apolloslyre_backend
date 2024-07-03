8e64fc62b9c4560f1284150176c18e15c61560b1
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-07-03 23:56:34 +0530

    Print statements added

--- flaskr/model.py
+++ flaskr/model.py
@@ -6,5 +6,7 @@ tokenizer = None
 def init_model_and_tokenizer(model_path):
     global model
     global tokenizer
+    print(f"Loading model from {model_path}")
     model = BigBirdForSequenceClassification.from_pretrained(model_path)
     tokenizer = BigBirdTokenizer.from_pretrained(model_path)
+    print("Model loaded successfully")
